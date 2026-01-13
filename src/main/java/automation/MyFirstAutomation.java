444f03b04633e57f1c3dcd877173406a7aa50b6e
Itzhak Levy <itzhaklevydev@gmail.com>
2026-01-13 12:40:16 +0200

    Use CSS selectors for multi-class buttons and By.className for a single class

--- src/main/java/automation/MyFirstAutomation.java
+++ src/main/java/automation/MyFirstAutomation.java
@@ -2,6 +2,7 @@ package automation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstAutomation {
@@ -12,17 +13,24 @@ public class MyFirstAutomation {
 		driver.get("https://automation.co.il/tutorials/selenium/demo1/indexID.html");
 		Thread.sleep(2000);
 		
-		driver.findElement(By.id("firstname")).sendKeys("Itzhak");
+		driver.findElement(By.className("form-control")).sendKeys("Itzhak");
 		driver.findElement(By.id("lastname")).sendKeys("Levy");
 		driver.findElement(By.id("email")).sendKeys("myEmail@gmail.com");
 		Thread.sleep(2000);
+		
+		// Multy classes would require getting the element by cssSelector ( for only one class use: by className instead of cssSelector )   
+		WebElement buttonElement = driver.findElement(By.cssSelector(".btn.btn-next.btn-fill.btn-warning.btn-wd.btn-sm"));
 
-		driver.findElement(By.id("next")).click();
+		String buttonElementText = buttonElement.getText();
+		
+		System.out.println("buttonElementText : " + buttonElementText);
+		
+		driver.findElement(By.cssSelector(".btn.btn-next.btn-fill.btn-warning.btn-wd.btn-sm")).click();
 		Thread.sleep(2000);
 		
 		driver.findElement(By.id("Beginner")).click();
 		
-		driver.findElement(By.id("next")).click();
+		driver.findElement(By.cssSelector("[name='next']")).click();
 		Thread.sleep(2000);
 		
 		driver.findElement(By.id("streetname")).clear();
