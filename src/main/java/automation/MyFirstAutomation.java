f47941922a198c2ca5e71e96bf53057baffb04a9
Itzhak Levy <itzhaklevydev@gmail.com>
2026-01-13 11:48:00 +0200

    Add end-to-end Selenium automation for demo registration form flow

--- src/main/java/automation/MyFirstAutomation.java
+++ src/main/java/automation/MyFirstAutomation.java
@@ -1,47 +1,41 @@
 package automation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstAutomation {
-    public static void main(String[] args) throws InterruptedException {	// throws InterruptedException is to allow the delay
-        WebDriver driver = new ChromeDriver();	// Opens up a chrome tab
-        driver.manage().window().maximize();	// Maximize opened tab
-        driver.get("https://www.automation.co.il");	// Navigate to this URL
-        
-        String url = driver.getCurrentUrl();
-        if (url.contains("automation")) {
-        	System.out.println("url contains 'automation' ");
-        	
-        	String title = driver.getTitle();
-        	System.out.println("Title : " + title);
-        	Thread.sleep(4000);
-        	
-        	System.out.println("Getting page source for url : " + url);
-        	Thread.sleep(4000);
-        	
-        	String pageSource = driver.getPageSource();
-        	System.out.println("Page Source : " + pageSource);
-        	Thread.sleep(4000);
-        	
-        	System.out.println("Navitating to : http://www.google.com");
-        	Thread.sleep(4000);
-        	
-        	driver.get("http://www.google.com");
-        	Thread.sleep(4000);
-        	
-        	driver.navigate().back();
-        	Thread.sleep(4000);
-        	
-        	driver.navigate().forward();
-        	Thread.sleep(4000);
-        	
-        	driver.navigate().refresh();
-        	Thread.sleep(4000);
-        	
-        	System.out.println("Closing browser...");
-        }
-        
-        driver.quit();
-    }
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://automation.co.il/tutorials/selenium/demo1/indexID.html");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("firstname")).sendKeys("Itzhak");
+		driver.findElement(By.id("lastname")).sendKeys("Levy");
+		driver.findElement(By.id("email")).sendKeys("myEmail@gmail.com");
+		Thread.sleep(2000);
+
+		driver.findElement(By.id("next")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("Beginner")).click();
+		
+		driver.findElement(By.id("next")).click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("streetname")).clear();
+		driver.findElement(By.id("streetname")).sendKeys("Sheshet");
+		
+		driver.findElement(By.id("streetnumber")).sendKeys("5");
+		
+		driver.findElement(By.id("city")).sendKeys("Netania");
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("finish")).click();
+		Thread.sleep(2000);
+		
+		driver.quit();
+	}
 }
