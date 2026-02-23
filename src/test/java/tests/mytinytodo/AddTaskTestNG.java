ee767fe971612c42c23d21f67af9b656e5120c9d
Itzhak Levy <itzhaklevydev@gmail.com>
2026-02-23 14:39:30 +0200

    refactor: restructure project to standard Maven directory layout
    - Moved test classes from `src/main/java` to `src/test/java` for proper Maven lifecycle support.
    - Verified test execution via `mvn clean test` through the Surefire plugin.
    - Confirmed Allure results generation in the project root directory.
    - Maintained framework infrastructure (Pages, Base, Utils) in `src/main/java`.
    - Updated pom.xml to correctly reference the new test environment.

--- src/test/java/tests/mytinytodo/AddTaskTestNG.java
+++ src/test/java/tests/mytinytodo/AddTaskTestNG.java
@@ -1,7 +1,5 @@
 package tests.mytinytodo;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageobjects.mytinytodo.TasksPage;
--- src/test/java/tests/mytinytodo/CheckNumOfTasksTestNG.java
+++ src/test/java/tests/mytinytodo/CheckNumOfTasksTestNG.java
@@ -1,9 +1,7 @@
 package tests.mytinytodo;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageobjects.mytinytodo.TasksPage;
